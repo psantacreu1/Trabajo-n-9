0590bff1459ededd700d09919fb487121e7fe72e
psantacreu <pablosantacreu@yahoo.com.ar>
2022-11-27 23:36:41 -0300

    T9

---
+++ js/java.js
@@ -0,0 +1,9 @@
+alert('Hola Mundo')
+
+var pais = "Argentina";
+var continente = "de mi corazón";
+var antiguedad = "2022";
+var pais_y_antiguedad = pais + " " + antiguedad + " " + "," + continente;
+
+console.log(pais,continente,antiguedad);
+alert(pais_y_antiguedad);
